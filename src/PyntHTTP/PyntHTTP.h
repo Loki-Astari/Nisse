70b06a0865fa14dcb8edf19e828317fd48100f8e
Loki Astari <Loki.Astari@gmail.com>
2024-10-16 15:40:44 -0700

    Initial version of HTTP interface

--- src/PyntHTTP/PyntHTTP.h
+++ src/PyntHTTP/PyntHTTP.h
@@ -41,6 +41,7 @@ class Header
 
     public:
         std::vector<std::string>&   getHeader(std::string_view header);
+        bool                        hasHeader(std::string_view header);
         CIterator                   begin() const;
         CIterator                   end()   const;
 };
@@ -50,7 +51,7 @@ class Request
     Version             httpVersion();
     Method              method();
     URL const&          url();
-    std::string_view    httpRawRequest();   // GET / HTTP/1.1
+    std::string_view    httpRawRequest();   // GET /index.html HTTP/1.1
 
     Header const&       headers();
     Header const&       trailers();         // Trailers will return an empty Header() if body has not been read.
@@ -58,12 +59,33 @@ class Request
 
     std::istream&       body();             // Can be used to read the stream body.
                                             // It will auto eof() when no more data is available in the body.
+                                            // Note this stream will auto decode the incoming message body based
+                                            // on the 'content-encoding'
+};
+
+class Response
+{
+    void                setStatus(int code, std::string_view message);  // If not set default 200 OK
+                                                                        // Can be modified anytime up to the headers being sent.
+    Header&             headers();          // Headers will be locked after they are sent. By default this happens when data is sent to the body stream.
+    Header&             trailers();
+    void                done();             // Manually indicate complete. Done automatically by destructor.
+                                            // But processes can use this to flush stream immediately if desired.
+
+    std::ostream&       body();             // Format of body is determined by the 'content-encoding/content-length' header fields.
+                                            // User will simply write to the stream the stream will automatically encode the data.
+                                            // Note: Headers will be locked after you start writing to the body stream and thus can
+                                            //       once writing commences the content-encoding/content-length can not be changed.
+
+    bool                isHeadersSent();
+    bool                isDone();
 };
 
 class PyntHTTP: public Pynt
 {
     public:
         virtual PyntResult handleRequest(std::iostream& stream) override;
+        virtual PyntResult processRequest(Request const& request, Response& response) = 0;
 };
 
 }
