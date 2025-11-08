f511ad2d427a97d8285e290fc11bb64b5a6c288d
Loki Astari <Loki.Astari@gmail.com>
2025-11-07 18:47:00 -0800

    Add appropriate tests

---
+++ src/NisseServer/test/NisseServerTimerTest.cpp
@@ -0,0 +1,40 @@
+#include "gtest/gtest.h"
+#include <thread>
+#include "NisseServer.h"
+
+using namespace ThorsAnvil::Nisse::Server;
+
+class TestAction: public ThorsAnvil::Nisse::Server::TimerAction
+{
+    int& value;
+    public:
+        TestAction(int& value)
+            : value(value)
+        {}
+        virtual void handleRequest(int timerId) override
+        {
+            value += 5;
+        }
+};
+
+TEST(NisseServerTimerTest, TestTimer)
+{
+    int                 value = 0;
+    NisseServer         server;
+    TestAction          action(value);
+
+    using namespace std::chrono_literals;
+    server.addTimer(2s, action);
+
+    auto work = [&server](){server.run();};
+    std::jthread    test(work);
+
+    EXPECT_EQ(0, value);
+    std::this_thread::sleep_for(1s);
+    EXPECT_EQ(0, value);
+    std::this_thread::sleep_for(2s);
+    EXPECT_EQ(5, value);
+
+    server.stop();
+}
+
