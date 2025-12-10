70557383de8d13b587cf76f2ed9784b68e620890
Loki Astari <Loki.Astari@gmail.com>
2025-12-10 13:48:26 -0800

    Improve tests

--- src/NisseHTTP/test/MugServerTest.cpp
+++ src/NisseHTTP/test/MugServerTest.cpp
@@ -140,21 +140,17 @@ class HTTPResponse
 
         void recv(std::istream& stream)
         {
-        std::cerr << "HTTPResponse 1\n";
             std::string     version;
             if (!(stream >> version >> code && std::getline(stream, message) && version == "HTTP/1.1")) {
-                std::cerr << "Bad Input\n";
+                ThorsLogDebug("HTTPResponse", "recv", "Bad Input");
                 return;
             }
-        std::cerr << "HTTPResponse 2\n";
             // Remove the '\r'
             message.resize(message.size() - 1);
 
-        std::cerr << "HTTPResponse 3\n";
             std::string     line;
             std::string     header;
             std::string     value;
-        std::cerr << "HTTPResponse 4\n";
             while (std::getline(stream, line)) {
                 if (line == "\r") {
                     break;
@@ -169,55 +165,37 @@ class HTTPResponse
                     headers[header] = value;
                 }
             }
-        std::cerr << "HTTPResponse 5\n";
             std::string    length   = std::string((*this)["content-length"]);
             if (length.size() != 0) {
                 readData(stream, std::stoul(length));
             }
-        std::cerr << "HTTPResponse 6\n";
             std::string_view    encoding = (*this)["transfer-encoding"];
             if (encoding == "chunked") {
                 readChunked(stream);
             }
-        std::cerr << "HTTPResponse DONE\n";
         }
     private:
         void readData(std::istream& stream, std::size_t size)
         {
-        std::cerr << "Read Data: " << size << "\n";
             body.resize(size);
-        std::cerr << "Reading\n";
             stream.read(&body[0], size);
-        std::cerr << "DONE\n";
         }
         void readChunked(std::istream& stream)
         {
-        std::cerr << "Read Chunked\n";
             std::string     chunkSizeStr;
             while (std::getline(stream, chunkSizeStr) && chunkSizeStr.back() == '\r') {
-        std::cerr << "\nGot Chunk: >\n" << chunkSizeStr << "<\n\nS: " << chunkSizeStr.size() << "\n";
-        for (int loop = 0; loop < chunkSizeStr.size(); ++loop) {
-            std::cerr << "C: " << loop << " " << ((int)chunkSizeStr[loop]) << "\n";
-        }
                 std::size_t     chunkSize = std::stoul(chunkSizeStr, nullptr, 16);
-        std::cerr << "Chunk Size: " << chunkSize << "\n";
                 std::size_t     bodySize = body.size();
                 body.resize(bodySize + chunkSize);
-        std::cerr << "About to read Chunk: " << chunkSize << "\n";
-        if (chunkSize != 0) {
+                if (chunkSize != 0) {
                     stream.read(&body[bodySize], chunkSize);
-        }
-        std::cerr << "Read Done: >" << std::string(&body[bodySize], &body[bodySize + chunkSize]) << "<\n";
-        std::cerr << "Avail: " << stream.rdbuf()->in_avail() << "\n";
-        char buf[2];
+                }
+                char buf[2];
                 stream.read(buf, 2);
-        std::cerr << "Ignored the new line\n";
                 if (chunkSize == 0) {
-            std::cerr << "Breaking\n";
                     break;
                 }
             }
-        std::cerr << "Read Chunk DONE\n";
         }
 
 
@@ -237,23 +215,20 @@ class MugServer: public NisServer::NisseServer
 
     void handleRequestPath(NisHttp::Request& request, NisHttp::Response& response)
     {
-        std::error_code ec;
-        FS::path   filePath = FS::canonical(FS::path{"./test/data/pages/page1"}, ec);
+        std::error_code         ec;
+        FS::path                filePath = FS::canonical(FS::path{"./test/data/pages/page1"}, ec);
+        TASock::SocketStream    file{TASock::Socket{TASock::FileInfo{filePath.string(), TASock::FileMode::Read}, TASock::Blocking::No}};
 
-        TASock::SocketStream    file{TASock::Socket{TASock::FileInfo{filePath.string(), TASock::FileMode::Read}, TASock::Blocking::Yes}};
-       // TASock::SocketStream    file{TASock::Socket{TASock::FileInfo{filePath.string(), TASock::FileMode::Read}, TASock::Blocking::No}};
-       // NisServer::AsyncStream  async(file, request.getContext(), NisServer::EventType::Read);
-
-        std::ostream& output = response.body(NisHttp::Encoding::Chunked);
-        std::string line;
-        std::cerr << "Reading Lines\n";
-        while (std::getline(file, line)) {
-            std::cerr << "Got Line: >" << line << "<\n";
-            output << line << "\n";
+        if (!file) {
+            // Can't open file for some reason.
+            // Then we indicate a 404.
+            response.setStatus(404);
+            return;
         }
-        std::cerr << "FLUSHING\n";
-        output << std::flush;
-        std::cerr << "DONE\n";
+        // Otherwise mark the file stream as Async (thus if the disk is slow we will be de-shcheduled for other work)
+        // and simply stream the file to the output stream.
+        NisServer::AsyncStream  async(file, request.getContext(), NisServer::EventType::Read);
+        response.body(NisHttp::Encoding::Chunked) << file.rdbuf();
     }
 
     public:
@@ -288,50 +263,32 @@ TEST(MugServer, ServiceRunAddServerWithFileValidateWorks)
         server.run(
                 [&latch](){latch.count_down();}
         );
-        std::cerr << "Run Exit\n";
         waitForExit.count_down();
-        std::cerr << "Latch Signalled\n";
     };
 
     LocalJthread     serverThread(work);
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 1\n";
     latch.wait();
 
     // Talk to server.
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 2\n";
     ThorsAnvil::ThorsSocket::SocketStream socketData({"localhost", 8070});
 
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 3\n";
     socketData << HTTPSend(SendType::GET, SendVersion::HTTP1_1, "localhost", "/files/page1");
 
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 4\n";
     HTTPResponse   response;
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 5\n";
     socketData >> response;
 
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 6\n";
     ASSERT_EQ("Data for page 1\n", response.getBody());
 
     // Touch the control point to shut down the server.
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 7\n";
     ThorsAnvil::ThorsSocket::SocketStream       socket({"localhost", 8079});
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 8\n";
     ThorsAnvil::Nisse::HTTP::HeaderResponse   headers;
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 9\n";
     headers.add("host", "localhost");
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 10\n";
     headers.add("content-length", "0");
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 11\n";
     ThorsAnvil::Nisse::HTTP::ClientRequest  request(socket, "localhost:/?command=stophard");
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 12\n";
     request.addHeaders(headers);
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 13\n";
     request.flushRequest();
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 14\n";
     std::cerr << "Request Flushed\n";
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 15\n";
     waitForExit.wait();
-std::cerr << "ServiceRunAddServerWithFileValidateWorks: 16\n";
     std::cerr << "Latch removed\n";
 }
 
